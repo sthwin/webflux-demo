fb297c011de613a032316a574cb160ba7a66dac1
sthwin <sthwin@naver.com>
2020-11-08 21:26:50 +0900

    spring-batch sample 작성중

--- src/main/java/com/sthwin/webflux/scheduler/JobScheduler.java
+++ src/main/java/com/sthwin/webflux/scheduler/JobScheduler.java
@@ -3,7 +3,6 @@ package com.sthwin.webflux.scheduler;
 import com.sthwin.webflux.config.batch.BatchConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.JobRegistry;
